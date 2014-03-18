f2eeeeea1433d24d39ffe07eb237e00cd7e74aed
Jeremy Brudvik <jeremy@brudvik.me>
2014-03-17 23:51:41 -0700

    Implemented create command

--- bin/baller.js
+++ bin/baller.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var _ = require('underscore');
 var fs = require('fs');
 var handlebars = require('handlebars');
 var path = require('path');
@@ -15,8 +16,58 @@ program
   .command('create <name>')
   .description('Create a new, empty ball in a new directory')
   .action(function (name) {
-    console.log('creating: ' + name + '\n');
-    console.log(renderReadme(name));
+    var errorMessage = 'Could not create ball';
+
+    // Create a new directory (fail and exist if dir exists or other failure)
+    try {
+      fs.mkdirSync(name);
+    } catch (e) {
+      if (e.errno === 47) {
+        errorMessage += ': Directory "' + name + '" already exists';
+      }
+      console.log(errorMessage);
+      process.exit(1);
+    }
+
+    // Copy `files` file over to directory
+    try {
+      var filesName = 'files';
+      fs.createReadStream(getResourcePath(filesName))
+        .pipe(fs.createWriteStream(path.join(name, filesName)));
+    } catch (e) {
+      errorMessage += ': `files` creation failed';
+      console.log(errorMessage);
+      process.exit(1);
+    }
+
+    // Write rendered template to directory (as README.md)
+    try {
+      var readmePath = path.join(name, 'README.md');
+      var readme = renderReadme(name);
+      fs.writeFileSync(readmePath, readme);
+    } catch (e) {
+      errorMessage += ': README creation failed';
+      console.log(errorMessage);
+      process.exit(1);
+    }
+
+    // Copy scripts to directory
+    try {
+      var scripts = fs.readdirSync(getScriptPath());
+      _.each(scripts, function (script) {
+        var original = getScriptPath(script);
+        var copy = path.join(name, script);
+        fs.createReadStream(original)
+          .pipe(fs.createWriteStream(copy));
+        fs.chmodSync(copy, 0755);
+      });
+    } catch (e) {
+      errorMessage += ': scripts creation failed';
+      console.log(errorMessage);
+      process.exit(1);
+    }
+
+    console.log('Created "' + name + '" ball');
   });
 
 program
@@ -63,16 +114,36 @@ if (!program.args.length) {
 }
 
 
+// Return absolute path for module-owned file
+function getModulePath(name, dir) {
+  var path = __dirname + '/../' + dir;
+  if (name) {
+    path += '/' + name;
+  }
+  return path;
+}
+
+// Return path for template
+function getTemplatePath(template) {
+  return getModulePath(template, 'templates');
+}
+
+// Return path for resource
+function getResourcePath(resource) {
+  return getModulePath(resource, 'resources');
+}
+
+// Return path for script
+function getScriptPath(script) {
+  return getModulePath(script, 'scripts');
+}
+
+
 // Render template resource with given context and return result
 function renderTemplate(template, context) {
   var source = fs.readFileSync(getTemplatePath(template)).toString();
   var compiledTemplate = handlebars.compile(source);
   return compiledTemplate(context);
-}
-
-// Return path for template
-function getTemplatePath(template) {
-  return __dirname + '/../templates/' + template;
 }
 
 // Render README for current user with given ball name and return result
