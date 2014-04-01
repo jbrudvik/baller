47610d35d24bec0dd1dfe57efbfba07e4d9b26fb
Jeremy Brudvik <jeremy@brudvik.me>
2014-03-31 22:02:50 -0700

    Refactored side effects to bin from lib
    Instead of outputting and exiting processes in lib code, throw errors
    and let outputting/exiting be done by bin code. This makes use of lib
    code (including in tests) much easier.

--- test/test.js
+++ test/test.js
@@ -9,4 +9,24 @@ describe('Baller', function () {
   it('should have create function', function () {
     expect(baller.create).to.exist;
   });
+
+  it('should have init function', function () {
+    expect(baller.init).to.exist;
+  });
+
+  it('should have update function', function () {
+    expect(baller.update).to.exist;
+  });
+
+  it('should have unball function', function () {
+    expect(baller.unball).to.exist;
+  });
+
+  it('should have deploy function', function () {
+    expect(baller.deploy).to.exist;
+  });
+
+  it('should fail to create ball without name', function () {
+    expect(baller.create).to.throw(/name/);
+  });
 });
