0e20012067e6f2796d5802e99aaa506896b3a5df
Jeremy Brudvik <jeremy@brudvik.me>
2014-04-09 22:28:20 -0700

    Added tests for Baller#init

--- test/index.js
+++ test/index.js
@@ -12,6 +12,26 @@ var baller = require('../lib');
 var pkg = require('../package.json');
 
 
+// Expected Baller files and directories
+var BALLER_META_DIR = '.baller';
+var VERSION_FILE = 'version';
+var README_FILE = 'README.md';
+var LICENSE_FILE = 'LICENSE';
+var FILES_FILE = 'files';
+var BALLER_SCRIPTS = [
+  'backup',
+  'install',
+  'uninstall',
+  'update',
+  'pre-install',
+  'post-install',
+  'remove-backups',
+  'restore',
+  'pre-update',
+  'post-update'
+];
+
+
 describe('Baller', function () {
 
   // Use a mock filesystem so these tests do not write to the actual
@@ -31,41 +51,43 @@ describe('Baller', function () {
   // Restore original filesystem. Don't keep filesystem changes across tests.
   afterEach(mockfs.restore);
 
+
   describe('API', function () {
-    it('should include create', function () {
+    it('includes create', function () {
       expect(baller.create).to.exist;
     });
 
-    it('should include init', function () {
+    it('includes init', function () {
       expect(baller.init).to.exist;
     });
 
-    it('should include update', function () {
+    it('includes update', function () {
       expect(baller.update).to.exist;
     });
 
-    it('should include unball', function () {
+    it('includes unball', function () {
       expect(baller.unball).to.exist;
     });
 
-    it('should include deploy', function () {
+    it('includes deploy', function () {
       expect(baller.deploy).to.exist;
     });
   });
 
+
   describe('#create', function () {
     var name = 'foo';
 
-    it('should fail to create ball without name', function () {
+    it('fails to create ball without name', function () {
       expect(baller.create).to.throw(/name/i);
     });
 
-    it('should fail to create ball if directory with name already exists', function () {
+    it('fails to create ball if directory with name already exists', function () {
       fs.mkdir(name);
       expect(baller.create.bind(baller, name)).to.throw(/directory/i);
     });
 
-    it('should return success message on success', function () {
+    it('returns success message on success', function () {
       expect(baller.create(name)).to.match(/create/i);
     });
 
@@ -79,46 +101,35 @@ describe('Baller', function () {
       });
 
       it('which includes Baller metadata', function () {
-        expect(fs.existsSync(path.join(name, '.baller'))).to.be.true;
+        expect(fs.existsSync(path.join(name, BALLER_META_DIR))).to.be.true;
       });
 
       it('which has correct Baller version metadata', function () {
-        var versionFilePath = path.join(name, '.baller', 'version');
+        var versionFilePath = path.join(name, BALLER_META_DIR, VERSION_FILE);
         var ballVersion = fs.readFileSync(versionFilePath).toString();
         expect(ballVersion).to.equal(pkg.version);
       });
 
       it('which includes a README', function () {
-        expect(fs.existsSync(path.join(name, 'README.md'))).to.be.true;
+        expect(fs.existsSync(path.join(name, README_FILE))).to.be.true;
       });
 
       it('which includes a `files` file', function () {
-        expect(fs.existsSync(path.join(name, 'files'))).to.be.true;
+        expect(fs.existsSync(path.join(name, FILES_FILE))).to.be.true;
       });
 
       it('which includes all Baller scripts', function () {
-        var scripts = [
-          'backup',
-          'install',
-          'uninstall',
-          'update',
-          'pre-install',
-          'post-install',
-          'remove-backups',
-          'restore',
-          'pre-update',
-          'post-update'
-        ];
-        _.each(scripts, function (script) {
+        _.each(BALLER_SCRIPTS, function (script) {
           expect(fs.existsSync(path.join(name, script))).to.be.true;
         });
       });
     });
   });
 
+
   describe('#init', function () {
 
-    // baller.init uses process.cwd initalize the current directory as a ball.
+    // baller.init uses process.cwd initialize the current directory as a ball.
     // Create a new directory and "change" the current working directory to it
     // by stubbing process.cwd to return the path of the new directory.
     beforeEach(function () {
@@ -134,13 +145,153 @@ describe('Baller', function () {
       process.cwd.restore();
     });
 
-    it('should return success message on success', function () {
+    it('fails to initialize ball if directory is already a ball', function () {
+      baller.init();
+      expect(baller.init).to.throw(/already/i);
+    });
+
+    it('returns success message on success', function () {
       expect(baller.init()).to.match(/init/i);
     });
 
-    it('should fail to initialize ball if directory is already a ball', function () {
-      baller.init();
-      expect(baller.init).to.throw(/already/i);
+    describe('initializes a new ball', function () {
+      beforeEach(baller.init);
+
+      it('which includes Baller metadata', function () {
+        expect(fs.existsSync(BALLER_META_DIR)).to.be.true;
+      });
+
+      it('which has correct Baller version metadata', function () {
+        var versionFilePath = path.join(BALLER_META_DIR, VERSION_FILE);
+        var ballVersion = fs.readFileSync(versionFilePath).toString();
+        expect(ballVersion).to.equal(pkg.version);
+      });
+
+      it('which includes a README', function () {
+        expect(fs.existsSync(README_FILE)).to.be.true;
+      });
+
+      it('which includes a `files` file', function () {
+        expect(fs.existsSync(FILES_FILE)).to.be.true;
+      });
+
+      it('which includes all Baller scripts', function () {
+        _.each(BALLER_SCRIPTS, function (script) {
+          expect(fs.existsSync(script)).to.be.true;
+        });
+      });
+    });
+
+
+    describe('when called in a directory containing no files', function () {
+      beforeEach(baller.init);
+
+      it('initializes a ball with no entries in `files` file', function () {
+        var filesFileContents = fs.readFileSync(FILES_FILE).toString();
+        expect(filesFileContents.trim()).to.be.empty;
+      });
+    });
+
+
+    describe('when called in a directory containing files', function () {
+      var existingFiles = [
+        'one',
+        'two',
+        'three'
+      ];
+
+      beforeEach(function () {
+        _.each(existingFiles, function (existingFile) {
+          fs.writeFile(existingFile);
+        });
+        baller.init();
+      });
+
+      it('initializes a ball with entries for each existing file in `files` file', function () {
+        var filesFileContents = fs.readFileSync(FILES_FILE).toString();
+        var filesFileEntries = filesFileContents.trim().split(/\s+/);
+        expect(filesFileEntries).to.have.members(existingFiles);
+      });
+    });
+
+
+    describe('when called in a directory containing directories', function () {
+      var existingDirs = [
+        'foo',
+        'bar',
+        'baz'
+      ];
+
+      beforeEach(function () {
+        _.each(existingDirs, function (existingDir) {
+          fs.mkdirSync(existingDir);
+        });
+        baller.init();
+      });
+
+      it('initializes a ball with no entries in `files` file', function () {
+        var filesFileContents = fs.readFileSync(FILES_FILE).toString();
+        expect(filesFileContents.trim()).to.be.empty;
+      });
+    });
+
+
+    describe('when called in a directory containing ignored files', function () {
+      var nonIgnoredFiles = [
+        'one',
+        'two',
+        'three'
+      ];
+
+      var ignoredFiles = [
+        README_FILE,
+        LICENSE_FILE,
+        FILES_FILE
+      ].concat(BALLER_SCRIPTS);
+
+      beforeEach(function () {
+        var existingFiles = nonIgnoredFiles.concat(ignoredFiles);
+        _.each(existingFiles, function (existingFile) {
+          fs.writeFile(existingFile);
+        });
+        baller.init();
+      });
+
+      it('initializes a ball with entries for all non-ignored files only `files` file', function () {
+        var filesFileContents = fs.readFileSync(FILES_FILE).toString();
+        var filesFileEntries = filesFileContents.trim().split(/\s+/);
+        expect(filesFileEntries).to.have.members(nonIgnoredFiles);
+      });
+    });
+
+
+    describe('when called in a directory containing files and directories', function () {
+      var existingFiles = [
+        'one',
+        'two',
+        'three'
+      ];
+      var existingDirs = [
+        'foo',
+        'bar',
+        'baz'
+      ];
+
+      beforeEach(function () {
+        _.each(existingFiles, function (existingFile) {
+          fs.writeFile(existingFile);
+        });
+        _.each(existingDirs, function (existingDir) {
+          fs.mkdirSync(existingDir);
+        });
+        baller.init();
+      });
+
+      it('initializes a ball with entries for all files only in `files` file', function () {
+        var filesFileContents = fs.readFileSync(FILES_FILE).toString();
+        var filesFileEntries = filesFileContents.trim().split(/\s+/);
+        expect(filesFileEntries).to.have.members(existingFiles);
+      });
     });
   });
 });
